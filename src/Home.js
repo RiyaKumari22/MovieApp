6181cf515092c875d6e03bca6a06bc3a1656f6e2
Riya Kumari <riya.kumari.4rmk@gmail.com>
2025-02-05 11:26:27 +0530

    Update chatbot functionality

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Movies from './Movies';
+import './Home.css';
+import Search from './Search';
 
-import Movies from './Movies'
-import Search from './Search'
 function Home() {
-  
+  const [isChatbotVisible, setIsChatbotVisible] = useState(false);
+
+  const toggleChatbot = () => {
+    setIsChatbotVisible(!isChatbotVisible);
+  };
+
   return (
     <>
-    <div>
-      <Search/>
-      <Movies/>
-    </div>
-   
+      <div>
+        <Search />
+        <Movies />
+
+        {/* Button to toggle chatbot visibility */}
+        <button 
+          id="chatbot-btn" 
+          className="chatbot-btn"
+          onClick={toggleChatbot}
+        >
+          Chat
+        </button>
+
+        {/* Chatbot iframe container */}
+        {isChatbotVisible && (
+          <div id="chatbot-container" className="chatbot-container">
+            <iframe
+              src="https://www.chatbase.co/chatbot-iframe/lkOnzr3d2Ybc_iGkFPyfs"
+              width="100%"
+              height="500px"
+              frameBorder="0"
+            ></iframe>
+          </div>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
